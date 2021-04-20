d00030310a9d13fa992ef801e34626add0b3fce2
sgarg03 <sat.garg03@gmail.com>
2021-04-19 19:25:43 -0700

    Created layout for Home page :construction:

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,132 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: PharmaConnect(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class PharmaConnect extends StatefulWidget {
+  @override
+  _PharmaConnectState createState() => new _PharmaConnectState();
+}
+
+class _PharmaConnectState extends State<PharmaConnect> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+        child: Stack(
+          alignment: Alignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            //Pharma Text
+            Container(
+              alignment: Alignment(-0.45, -0.55),
+              child: RichText(
+                text: TextSpan(
+                  text: "Pharma",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 50.0,
+                      color: Colors.black),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            //Connect Text
+            Container(
+              //top: 240,
+              // left: 130,
+              alignment: Alignment(0.45, -0.35),
+              child: RichText(
+                text: TextSpan(
+                  text: "Connect",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 50.0,
+                      color: Colors.black),
+                ),
+              ),
+            ),
+            //Register Text
+            Container(
+              //top: 360,
+              alignment: Alignment(0, 0.2),
+              child: RichText(
+                text: TextSpan(
+                  text: "Register",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 35.0,
+                      color: Colors.black),
+                ),
+              ),
+            ),
+            //Log In Text and Button
+            Container(
+              alignment: Alignment(0, 0.95),
+              child: GestureDetector(
+                child: RichText(
+                  text: TextSpan(
+                    text: "Log In",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 25.0,
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  //Push to Login Screen
+                },
+              ),
+            ),
+            //Button For Pharmacist Registration
+            Container(
+              alignment: Alignment(0, 0.4),
+              child: SizedBox(
+                width: 300,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    //Send to Pharmacist Sign Up Page
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Pharmacist",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            //Button For Pharmacy Registration
+            Container(
+              alignment: Alignment(0, 0.6),
+              child: SizedBox(
+                width: 300,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    //Send to Pharmacy Sign Up Page
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Pharmacy",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
